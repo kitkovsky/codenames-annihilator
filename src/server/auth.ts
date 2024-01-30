c2a6d9291ae1c00109a9d5324ca9adbac099f60e
kitkovsky <okitkowski114@gmail.com>
2024-01-30 16:14:36 +0100

    upgrade next-auth to v5

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,19 +1,11 @@
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-  type Session,
-} from 'next-auth'
+import NextAuth from 'next-auth'
+import { type DefaultSession, type Session } from 'next-auth'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
-import { eq, and } from 'drizzle-orm'
-import type { Adapter } from 'next-auth/adapters'
 
 import { env } from '@/env'
 import { db } from '@/server/db'
-import { users } from '@/server/db/schema/users'
-import { accounts } from '@/server/db/schema/auth'
 
 declare module 'next-auth' {
   interface Session extends DefaultSession {
@@ -23,47 +15,20 @@ declare module 'next-auth' {
   }
 }
 
-// next-auth drizzle adapter doesn't support async sqlite clients (like turso's libsql),
-// which caused errors on consecutive logins, so this is a workaround
-// https://github.com/nextauthjs/next-auth/issues/8377#issuecomment-1694720111
-// @ts-expect-error ignore for now
-const AsyncDrizzleAdapter: Adapter = {
-  ...DrizzleAdapter(db),
-  async getUserByAccount(providerAccountId) {
-    const results = await db
-      .select()
-      .from(accounts)
-      .leftJoin(users, eq(users.id, accounts.userId))
-      .where(
-        and(
-          eq(accounts.provider, providerAccountId.provider),
-          eq(accounts.providerAccountId, providerAccountId.providerAccountId),
-        ),
-      )
-      .get()
-
-    return results?.user ?? null
-  },
-  async createUser(data) {
-    return await db
-      .insert(users)
-      .values({ ...data, id: crypto.randomUUID() })
-      .returning()
-      .get()
-  },
-}
-
-export const authOptions: NextAuthOptions = {
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
   callbacks: {
-    session: ({ session, user }) => ({
+    session: ({ session }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: session.user.id,
       },
     }),
   },
-  adapter: AsyncDrizzleAdapter,
+  adapter: DrizzleAdapter(db),
   providers: [
     GoogleProvider({
       clientId: env.OAUTH_GOOGLE_CLIENT_ID,
@@ -75,7 +40,6 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   debug: env.NODE_ENV === 'development',
-}
+})
 
-export const getServerAuthSession = (): Promise<Session | null> =>
-  getServerSession(authOptions)
+export const getServerAuthSession = (): Promise<Session | null> => auth()
